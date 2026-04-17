022c95f32858c04f15b7183c1bba4b7791d844da
方便面 <85048235+fangbm@users.noreply.github.com>
2026-04-17 14:40:13 +0800

    更新 docusaurus.config.js
    更改项目仓库链接

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Paper2Gal', // Usually your GitHub org/user name.
+  projectName: 'p2g_support_test', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/fangbm/p2g_support_test/doc'
         },
         blog: false,
         theme: {
